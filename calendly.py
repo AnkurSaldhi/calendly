f6c7e1209c0fbf3e88c7c7f438c81c161223e95d
Ankur Saldhi <ankur479225@gmail.com>
2024-08-28 18:07:49 +0530

    change debug to false

--- calendly.py
+++ calendly.py
@@ -165,4 +165,4 @@ def reschedule_availability():
     return jsonify({"message": "Availability rescheduled successfully"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
